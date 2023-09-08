98daeefe7e88de2e5a6e08ad909d098cd09b5059
bandiiskola <nandor.bere@gmail.com>
2023-09-08 08:34:12 +0200

    No Objektum

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,20 @@
 var data=[]
 var varakozo=[]
-var ferfi=document.getElementById("ferfi")
-var no=document.getElementById("no")
 
 function Katt()
 {
-    if(document.getElementById(ferfi.checked))
+    if(document.getElementById("ferfi").checked)
     {
-        varakozo.Add("ferfi")
+        varakozo.push(document.getElementById("ferfi").value)
     }
+    else if(document.getElementById("no").checked)
+    {
+        varakozo.push(document.getElementById("no").value)
+    }
+    if (document.getElementById("eletkor").value!=0&&document.getElementById("eletkor").value>=1)
+    {
+        varakozo.push(document.getElementById("eletkor").value)
+    }
+    data.push(varakozo)
+    varakozo.Clear
 }
