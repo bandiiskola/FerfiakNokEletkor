600d96a81ea69126bd9bfa3cdf4eb07e33ff6b9d
bandiiskola <nandor.bere@gmail.com>
2023-09-08 08:20:42 +0200

    HTML

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+var data=[]
+var varakozo=[]
+var ferfi=document.getElementById("ferfi")
+var no=document.getElementById("no")
+
+function Katt()
+{
+    if(document.getElementById(ferfi.checked))
+    {
+        varakozo.Add("ferfi")
+    }
+}
