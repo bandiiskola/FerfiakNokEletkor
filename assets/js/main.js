1669b9953fac23e6bac178adf9ff205882e7bbfc
bandiiskola <nandor.bere@gmail.com>
2023-09-08 10:11:41 +0200

    Javit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,51 @@
 var data=[]
-var varakozo=[]
+var n=""
+var ferfiszam=0
+var noszam=0
+var ferfiatlag=0
+var noatlag=0
+var ferfieletkor=0
+var noeletkor=0
 
 function Katt()
 {
+    n=""
     if(document.getElementById("ferfi").checked)
     {
-        varakozo.push(document.getElementById("ferfi").value)
+        n=document.getElementById("ferfi").value
     }
     else if(document.getElementById("no").checked)
     {
-        varakozo.push(document.getElementById("no").value)
+        n=document.getElementById("no").value
     }
-    if (document.getElementById("eletkor").value!=0&&document.getElementById("eletkor").value>=1)
+    if (n!="" && document.getElementById("eletkor").value!=0 && document.getElementById("eletkor").value>=1)
     {
-        varakozo.push(document.getElementById("eletkor").value)
+        var obj = {
+            nem : n,
+            eletkor : parseInt(document.getElementById("eletkor").value)
+        }
+        data.push(obj)
     }
-    data.push(varakozo)
-    varakozo.Clear
+    console.log(data)
+    n=0
+    for(var i=0;i<data.length;i++)
+    {
+        if(data[i].nem=="ferfi")
+        {
+            ferfiszam++
+            ferfieletkor+=data[i].eletkor
+        }
+        else
+        {
+            noszam++
+            noeletkor+=data[i].eletkor
+        }
+    }
+    ferfiat
+    ferfiak.push(n)
+    nok.push(data.length-n)
+    ferfiak.push(ferfieletkor/n)
+    nok.push(noeletkor/data.length-n)
+    console.log(ferfiak)
+
 }
